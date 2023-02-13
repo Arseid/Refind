0bb3176f5daecf02ae0f6de87af5aeaea32366b3
Arseid <yvesrobert.estrada@gmail.com>
2023-02-13 14:46:03 +0100

    REM updated

--- src/components/game/scenes/Bedroom.jsx
+++ src/components/game/scenes/Bedroom.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Bedroom = () => {
     return (
         <div style={{width: 'fit-content', height: 'fit-content'}}>
-            <img style={{width: "200rem", height: "100rem"}} src={require('ressources/bedroom.jpg')} alt=''/>
+            <img style={{width: "100rem", height: "50rem"}} src={require('ressources/bedroom.jpg')} alt=''/>
         </div>
     )
 }
--- src/components/game/scenes/Garage.jsx
+++ src/components/game/scenes/Garage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Garage = () => {
     return (
         <div style={{width: 'fit-content', height: 'fit-content'}}>
-            <img style={{width: "200rem", height: "100rem"}} src={require('ressources/garage.jpg')} alt=''/>
+            <img style={{width: "100rem", height: "50rem"}} src={require('ressources/garage.jpg')} alt=''/>
         </div>
     )
 }
