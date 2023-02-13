e722cccce9d7a3d2de1bd1694c3ab3fb5fbc2e29
Arseid <yvesrobert.estrada@gmail.com>
2023-02-13 18:54:08 +0100

    Point item found

---
+++ src/components/game/scenes/Victory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Victory = () => {
+    return (
+        <div style={{width: 'fit-content', height: 'fit-content'}}>
+            <img style={{ width: "100rem", height: "50rem" }} src={require('ressources/victory.jpg')} alt='' />
+        </div>
+    )
+}
+
+export default Victory
