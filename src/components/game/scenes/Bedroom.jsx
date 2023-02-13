4ea66df99562f471c93c254ca068f8439b3c9609
Arseid <yvesrobert.estrada@gmail.com>
2023-02-13 13:22:02 +0100

    Scenes

--- src/components/game/scenes/Bedroom.jsx
+++ src/components/game/scenes/Bedroom.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Bedroom = () => {
     return (
-        <div>a</div>
+        <div style={{width: 'fit-content', height: 'fit-content'}}>
+            <img style={{width: "200rem", height: "100rem"}} src={require('ressources/bedroom.jpg')} alt=''/>
+        </div>
     )
 }
 
