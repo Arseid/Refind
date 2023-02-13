0d381dafe5a3ed3eecdf78844be406f77dcde8b7
Ryan-MARIN <95910660+Ryan-MARIN@users.noreply.github.com>
2023-02-13 19:35:07 +0100

    answer input retrieval and ui

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,21 @@ import refs from 'ressources/refs.json';
 console.log(refs);
 
 function App() {
-    const [answerValue, setAnswerValue] = useState('');
+    const [answerValue, setAnswerValue] = useState(null);
+    const [found, setFound] = useState(0);
 
-    function onValidateAnswer(e, action) {
+    function onValidateAnswer(e) {
         setAnswerValue(e.target.value);
-        if (action) {
-            e.target.value = '';
-            console.log(answerValue);
-        }
     }
+
+    // console.log(answerValue);
 
     return (
         <div className="App">
             <Layout
                 answerValue={answerValue}
                 onValidateAnswer={onValidateAnswer}
-                found={1}
+                found={found}
                 toFind={10}
             >
                 <Game answerValue={answerValue} />
--- src/components/layout/AnswerInput.jsx
+++ src/components/layout/AnswerInput.jsx
@@ -8,26 +8,18 @@ const AnswerInput = ({ answerValue, onValidateAnswer }) => {
         <Stack direction="row" sx={{ width: '100%', pb: '1rem' }}>
             <InputBase
                 id="answerInput"
-                onInput={(e) => {
+                onChange={(e) => {
                     onValidateAnswer(e);
-                }}
-                onKeyPress={(e) => {
-                    if (e.key === 'Enter') {
-                        onValidateAnswer(e, true);
-                    }
                 }}
                 placeholder="Avez-vous trouvé quelque chose...?"
                 style={{
                     backgroundColor: 'white',
-                    borderRadius: '2rem 0rem 0rem 2rem',
+                    borderRadius: '2rem',
                     padding: '0rem 1rem 0rem',
                     borderStyle: 'none',
                     flexGrow: 1,
                 }}
             />
-            <Button variant="contained" style={{ borderRadius: '0rem 2rem 2rem 0rem' }}>
-                <CheckOutlinedIcon />
-            </Button>
         </Stack>
     );
 };
