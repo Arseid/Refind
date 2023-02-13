f7dbe117c8331b7e682a666ddb80cfa9b235d6e4
Arseid <yvesrobert.estrada@gmail.com>
2023-02-13 13:53:40 +0100

    Different lvl on state change

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Bedroom from './scenes/Bedroom';
+import Street from './scenes/Steet';
+import Garage from './scenes/Garage';
+import 'styles/game.css';
 
 const Game = () => {
+    const [lvl, setLvl] = React.useState(1);
+
     return (
-        <div>a</div>
+        <div className='Game'>
+            <div className='Game-Background'>
+                {lvl===1 ? <Bedroom/> : ''}
+                {lvl===2 ? <Garage/> : ''}
+                {lvl===3 ? <Street/> : ''}
+                {lvl===4 ? 'GG T TRO FOR' : ''}
+            </div>
+        </div>
     )
 }
 
