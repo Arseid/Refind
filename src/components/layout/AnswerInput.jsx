316c7aefbe89267e2b3e3671c20b84310b8a1c3f
Ryan-MARIN <95910660+Ryan-MARIN@users.noreply.github.com>
2023-02-13 16:02:20 +0100

    layout

--- src/components/layout/AnswerInput.jsx
+++ src/components/layout/AnswerInput.jsx
@@ -5,7 +5,7 @@ import { Stack } from '@mui/system';
 
 const AnswerInput = ({ onValidateAnswer }) => {
     return (
-        <Stack direction="row" sx={{ width: '100%' }}>
+        <Stack direction="row" sx={{ width: '100%', pb: '1rem' }}>
             <InputBase
                 onKeyPress={(e) => {
                     if (e.key === 'Enter') {
@@ -15,13 +15,13 @@ const AnswerInput = ({ onValidateAnswer }) => {
                 placeholder="Avez-vous trouvé quelque chose...?"
                 style={{
                     backgroundColor: 'white',
-                    borderRadius: '100rem 0rem 0rem 100rem',
-                    padding: '0rem 0.5rem 0rem',
+                    borderRadius: '2rem 0rem 0rem 2rem',
+                    padding: '0rem 2rem 0rem',
                     borderStyle: 'none',
                     flexGrow: 1,
                 }}
             />
-            <Button variant="contained" style={{ borderRadius: '0rem 100rem 100rem 0rem' }}>
+            <Button variant="contained" style={{ borderRadius: '0rem 2rem 2rem 0rem' }}>
                 <CheckOutlinedIcon />
             </Button>
         </Stack>
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,15 +3,56 @@ import React from 'react';
 import AnswerInput from './AnswerInput';
 import Hint from './Hint';
 import Next from './Next';
+import logo from 'ressources/refind-logo.png';
+import { Box } from '@mui/material';
+import Progress from './Progress';
 
 const Layout = ({ onValidateAnswer }) => {
     return (
-        <Stack spacing={2} direction="row" alignItems={'top'}>
-            <Hint />
-            <div style={{ width: '100%', marginTop: '1rem' }}>
-                <AnswerInput onValidateAnswer={onValidateAnswer} />
-            </div>
-            <Next />
+        <Stack
+            spacing={2}
+            justifyContent={'top'}
+            sx={{
+                position: 'absolute',
+                width: '100vw',
+                height: '100vh',
+            }}
+        >
+            <Stack
+                spacing={2}
+                sx={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    p: '1rem',
+                }}
+            >
+                <Box
+                    component="img"
+                    sx={{
+                        content: `url(${logo})`,
+                    }}
+                    alt="logo-pokedex"
+                    width="20rem"
+                />
+                <Progress />
+            </Stack>
+            <div style={{ flexGrow: 1, minWidth: '1rem' }} />
+            <Stack spacing={2} direction="row" alignItems={'top'}>
+                <Hint />
+                <Stack
+                    style={{
+                        width: '100%',
+                        marginTop: '1rem',
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}
+                >
+                    <div style={{ width: '100%', marginTop: '1rem' }}>
+                        <AnswerInput onValidateAnswer={onValidateAnswer} />
+                    </div>
+                </Stack>
+                <Next />
+            </Stack>
         </Stack>
     );
 };
