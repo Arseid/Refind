ac6dfb61d760e148d2b5338366692581894f2ae2
Ryan-MARIN <95910660+Ryan-MARIN@users.noreply.github.com>
2023-02-13 18:17:22 +0100

    game in layout

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,9 @@ function App() {
     }
     return (
         <div className="App">
-            <Layout onValidateAnswer={onValidateAnswer}></Layout>
+            <Layout onValidateAnswer={onValidateAnswer}>
+                <Game />
+            </Layout>
         </div>
     );
 }
