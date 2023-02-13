3506e42f9d33a1551d7b42b7cd236af2e570f7c9
Arseid <yvesrobert.estrada@gmail.com>
2023-02-13 14:46:03 +0100

    REM updated

--- src/components/game/scenes/Steet.jsx
+++ src/components/game/scenes/Steet.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Street = () => {
     return (
         <div style={{width: 'fit-content', height: 'fit-content'}}>
-            <img style={{width: "200rem", height: "100rem"}} src={require('ressources/street.jpg')} alt=''/>
+            <img style={{width: "100rem", height: "50rem"}} src={require('ressources/street.jpg')} alt=''/>
         </div>
     )
 }
