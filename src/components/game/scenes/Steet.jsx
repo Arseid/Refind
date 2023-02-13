c203472b08923e2977f06949fe985dc09c45c7e4
Arseid <yvesrobert.estrada@gmail.com>
2023-02-13 13:22:02 +0100

    Scenes

--- src/components/game/scenes/Steet.jsx
+++ src/components/game/scenes/Steet.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Street = () => {
     return (
-        <div>a</div>
+        <div style={{width: 'fit-content', height: 'fit-content'}}>
+            <img style={{width: "200rem", height: "100rem"}} src={require('ressources/street.jpg')} alt=''/>
+        </div>
     )
 }
 
